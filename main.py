20fbada934a17f7a4de583479cdc50f8d99cd425
tanuj <tanujb522@gmail.com>
2025-04-28 18:13:35 +0530

    Initial commit for 4th sem platformer game project

---
+++ main.py
@@ -0,0 +1,200 @@
+import pygame
+import time
+import os
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("2D platformer")
+icon = pygame.image.load("assets/icon.png").convert_alpha()
+pygame.display.set_icon(icon)
+
+# set frame rate
+clock = pygame.time.Clock()
+FPS = 75
+
+# define game variables
+GRAVITY = 0.5
+
+# define player variables
+moving_right = False
+moving_left = False
+shoot = False
+
+
+arrow = pygame.image.load("assets/arrow.png").convert_alpha()
+
+def Draw_BG():
+    screen.fill((0, 0, 0))
+    pygame.draw.line(screen, (255, 255, 255), (0, SCREEN_HEIGHT - 50), (SCREEN_WIDTH, SCREEN_HEIGHT - 50), 5)
+    pygame.draw.rect(screen, (0, 255, 0), (0, SCREEN_HEIGHT - 50, SCREEN_WIDTH, 50))
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, char_type, scale=2, speed=5):
+        pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.speed = speed
+        self.direction = 1  # 1 for right, -1 for left
+        self.flip = False  # flip the image if moving left4
+        self.jump = False
+        self.in_air = False
+        self.velocity_y = 0
+        self.animation_list = []
+        self.index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+        temp_list = []
+
+        # load all images for the character
+        animation_types = ['idle', 'run', 'jump']
+        for animation in animation_types:
+            temp_list = []
+            num_of_frames = len(os.listdir(f'assets/{char_type}/{animation}'))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f'assets/{char_type}/{animation}/{i}.png').convert_alpha()
+                img = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale)))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self, moving_left, moving_right):
+        # reset movement variables
+        dx = 0
+        dy = 0
+        # assign the image to the sprite based on the direction of movement
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+        
+        # jumping
+        if self.jump and not self.in_air:
+            self.velocity_y = -10
+            self.jump = False
+            self.in_air = True
+        # apply gravity
+        # self.velocity_y += GRAVITY
+        # if self.velocity_y > 10:
+        #     self.velocity_y = 10
+
+        dy += self.velocity_y
+
+        # check for collision with the ground
+        if self.rect.bottom + dy > SCREEN_HEIGHT - 50:
+            dy = SCREEN_HEIGHT - 50 - self.rect.bottom
+            self.in_air = False
+            self.velocity_y = 0
+        else:
+            self.velocity_y += GRAVITY
+            if self.velocity_y > 10:
+                self.velocity_y = 10
+
+        # update the position of the sprite
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def update_animation(self):
+        # update animation
+        ANIMATION_COOLDOWN = 100
+        # check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.index += 1
+            if self.index >= len(self.animation_list[self.action]):
+                self.index = 0
+            self.image = self.animation_list[self.action][self.index]
+
+    def update_action(self, new_action):
+        if new_action != self.action:
+            self.action = new_action
+            self.index = 0
+            self.update_time = pygame.time.get_ticks()
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(
+            self.image, self.flip, False), self.rect)
+        pygame.draw.rect(screen, (255, 0, 0), self.rect, 2)
+
+
+class Arrow(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10
+        self.image = arrow
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+
+# create sprite groups
+arrow_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
+
+player = Player(200, 200, "player", 2, 5)
+enemy = Player(400, 200, "enemy", 2, 5)
+
+run = True
+while run:
+    # drawing Things
+    Draw_BG()
+    enemy.draw()
+    arrow_group.update()
+    arrow_group.draw(screen)
+
+    player.draw()
+    player.update_animation()
+
+    if player.alive:
+        if shoot:
+            arrow = Arrow(player.rect.centerx, player.rect.centery, player.direction)
+            arrow_group.add(arrow)
+            shoot = False
+        if player.in_air:
+            player.update_action(2)
+        elif moving_left or moving_right:
+            player.update_action(1)
+        else:
+            player.update_action(0)
+        player.move(moving_left, moving_right)
+
+    # check for key presses
+    for event in pygame.event.get():
+        # check for quit event
+        if event.type == pygame.QUIT:
+            run = False
+
+        # check for keydown event
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_f:
+                shoot = True
+            if event.key == pygame.K_SPACE and player.alive:
+                player.jump = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+        # check for keyup event
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_f:
+                shoot = False
+
+    # update the display and tick the clock
+    clock.tick(FPS)
+    pygame.display.flip()
+pygame.quit()
